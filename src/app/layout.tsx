2cd68e63c1aa90839af96d38be9c55249c6500bc
blazehp <blazehunterhp@gmail.com>
2025-01-06 11:56:36 -0400

    small adjustment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,6 @@ export default function RootLayout({
             <Link href="/">Home</Link>
             <Link href="/about">About</Link>
             <Link href="/services">Services</Link>
-            <Link href="/courses">Courses</Link>
             <Link href="/#contact">Contact</Link>
 
           </nav>
