47cf02e5f4d7a4ae7c61a93144329f396f89f472
aalibrahimi <166450703+aalibrahimi@users.noreply.github.com>
2025-03-06 05:08:46 -0800

    removed the gray bg in contact form section by removing section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -572,8 +572,8 @@ const HomePage = () => {
 
      {/* 1. CONTACT FORM IMPROVEMENTS */}
 
-<section id="contact" className="py-20 md:py-24">
-  <div className="container mx-auto px-4 md:px-8 lg:px-12">
+
+  <div className="container mx-auto px-4 md:px-8 lg:px-12 ">
     <div className="max-w-4xl mx-auto">
       <motion.div 
         className="text-center mb-12"
@@ -660,7 +660,7 @@ const HomePage = () => {
       </motion.div>
     </div>
   </div>
-</section>
+
 
       {/* CTA Section */}
       <section className="py-20 md:py-24 bg-gradient-to-b from-red-950/10 to-black">
