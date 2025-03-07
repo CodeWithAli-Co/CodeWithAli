22a5de8a699a29d3ae75acd5898906490daa280f
aalibrahimi <166450703+aalibrahimi@users.noreply.github.com>
2025-03-06 17:29:07 -0800

    created the services ux ui design page ( still needs to be tweaked but the purpose is to transform the web app asap)

--- src/app/(routes)/services/E-Commerse/page.tsx
+++ src/app/(routes)/services/E-Commerse/page.tsx
@@ -23,7 +23,7 @@ import {
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 
 // Animation variants
 const fadeIn = {
